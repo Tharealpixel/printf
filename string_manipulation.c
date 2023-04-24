adfd72becb9f4dd310e411f92d37b90c02096ca1
MosaabCHERKAOUI <mouschark@icloud.com>
2023-04-24 14:59:06 +0100

    adding some files

---
+++ string_manipulation.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * print_reversed - reverse then print string
+ * @arg: arg of the func
+ * Return: how many chars printed
+ */
+int print_reversed(va_list arg)
+{
+	int len;
+	char *str;
+	char *ptr;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		return (-1);
+	ptr = rev_string(str);
+	if (ptr == NULL)
+		return (-1);
+	for (len = 0; ptr[len] != '\0'; len++)
+		_putchar(ptr[len]);
+	free(ptr);
+	return (len);
+}
+
+/**
+ * rot13 - to rot13
+ * @list: string for converting
+ * Return: string converted
+ */
+int rot13(va_list list)
+{
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		return (-1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (str[i] == s[x])
+			{
+				_putchar(u[x]);
+				break;
+			}
+		}
+		if (x == 53)
+			_putchar(str[i]);
+	}
+	return (i);
+}
